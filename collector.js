f353ec5c8740799976457ed666627937c4e5c2c5
Francois Laplaud <francois.laplaud@sdis72.fr>
2016-10-05 17:47:13 +0200

    adding hashing func

--- collector.js
+++ collector.js
@@ -1,22 +1,24 @@
 const Request = require('request');
 const fs = require('fs');
 const beautify = require('js-beautify').js_beautify;
+const crypto = require('crypto');
+let filePrefix = 'agario.core.';
 
-let  filePrefix = 'agario.core.';
+function main() {
+    downloadLatestCore()
+        .then((code)=> {
+            return beautify(code);
+        })
 
-downloadLatestCore()
-    .then((code)=> {
-        return beautify(code);
-    })
-
-    .then((code)=> {
-        // console.log(code);
-        return code;
-    })
-    .then(saveCore)
-    .catch((err)=> {
-        console.log('Error : ', err)
-    });
+        .then((code)=> {
+            // console.log(code);
+            return code;
+        })
+        .then(saveCore)
+        .catch((err)=> {
+            console.log('Error : ', err)
+        });
+}
 
 
 function downloadLatestCore() {
@@ -43,33 +45,65 @@ function saveCore(code) {
     });
 }
 
+function hashFromFile(fileName) {
+    return new Promise(function (resolve, reject) {
+
+        var fd = fs.createReadStream(fileName);
+        var hash = crypto.createHash('sha1');
+        hash.setEncoding('hex');
+
+        fd.on('end', function () {
+            hash.end();
+            console.log(hash.read()); // the desired sha1sum
+            resolve(hash.read());
+        });
+
+        // read all file and pipe it (write it) to the hash object
+        fd.pipe(hash);
+    });
+
+}
+
+function hashFromString(str) {
+    var hash = crypto.createHash('sha1');
+    // change to 'binary' if you want a binary hash.
+    hash.setEncoding('hex');
+    // the text that you want to hash
+    hash.write(str);
+    // very important! You cannot read from the stream until you have called end()
+    hash.end();
+    // and now you get the resulting hash
+    var sha1sum = hash.read();
+    return sha1sum;
+}
 
 // from http://stackoverflow.com/a/24853826/1446677
-function getNewestFile(dir, files, callback) {
-    if (!callback) return;
-    if (!files || (files && files.length === 0)) {
-        callback();
-    }
-    if (files.length === 1) {
-        callback(files[0]);
-    }
-    var newest = {file: files[0]};
-    var checked = 0;
-    fs.stat(dir + newest.file, function (err, stats) {
-        newest.mtime = stats.mtime;
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            (function (file) {
-                fs.stat(file, function (err, stats) {
-                    ++checked;
-                    if (stats.mtime.getTime() > newest.mtime.getTime()) {
-                        newest = {file: file, mtime: stats.mtime};
-                    }
-                    if (checked == files.length) {
-                        callback(newest);
-                    }
-                });
-            })(dir + file);
+function getNewestFile(dir, files) {
+    return new Promise(function (resolve, reject) {
+        if (!files || (files && files.length === 0)) {
+            reject('no files');
         }
+        var newest = {file: files[0]};
+        var checked = 0;
+        fs.stat(dir + newest.file, function (err, stats) {
+            newest.mtime = stats.mtime;
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                (function (file) {
+                    fs.stat(file, function (err, stats) {
+                        ++checked;
+                        if (stats.mtime.getTime() > newest.mtime.getTime()) {
+                            newest = {file: file, mtime: stats.mtime};
+                        }
+                        if (checked == files.length) {
+                            resolve(newest);
+                        }
+                    });
+                })(dir + file);
+            }
+        });
     });
 }
+
+
+main();
